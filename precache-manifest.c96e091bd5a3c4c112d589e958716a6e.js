6d445fec0f33fc6ca0881c9e5c6c2d796317db41
Connor Moynahan <connor.moynahan@ge.com>
2019-12-17 14:25:50 -0500

    Updates

---
+++ precache-manifest.c96e091bd5a3c4c112d589e958716a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efe84829a086f3dca783edd92c31685",
+    "url": "/boxgame/index.html"
+  },
+  {
+    "revision": "d1d102e59d5294fa2b29",
+    "url": "/boxgame/static/css/main.5a66b3f1.chunk.css"
+  },
+  {
+    "revision": "19a32c36ed3a05c37cb3",
+    "url": "/boxgame/static/js/2.ff6f30ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/boxgame/static/js/2.ff6f30ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1d102e59d5294fa2b29",
+    "url": "/boxgame/static/js/main.eee81e9f.chunk.js"
+  },
+  {
+    "revision": "332f26bb580c299f42bf",
+    "url": "/boxgame/static/js/runtime-main.793811e3.js"
+  }
+]);
