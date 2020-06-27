9a35cecceb931a45609712e6958599fd044afc9f
Connor Moynahan <connormoynahan@julias-mbp.lan>
2020-06-26 20:38:48 -0400

    adding router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, BrowserRouter, IndexRoute } from 'react-router-dom';
+import {TestingSocket} from './components/TestingSocket';
 import { Game } from './components/Game';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render((
+/* <Router history = {browserHistory}> */
+    <Router>
+
+    <Route exact path = "/" component = {Game}/>
+       {/* <IndexRoute component = {Game} /> */}
+       <Route exact path="/testing" component={TestingSocket} />
+       {/* <Route path = "about" component = {About} />
+       <Route path = "contact" component = {Contact} /> */}
+    {/* </Route> */}
+ </Router>), document.getElementById("root"));
