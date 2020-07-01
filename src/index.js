6583ca9226f8611a13906b7dc418dd7dbcf65fab
Connor Moynahan <connor.moynahan@ge.com>
2020-07-01 13:48:52 -0400

    changing router base route

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render((
-/* <Router history = {browserHistory}> */
-    <Router basename={process.env.PUBLIC_URL}>
+   /* <Router history = {browserHistory}> */
+   <Router basename={`${process.env.PUBLIC_URL}/`}>
 
-    <Route exact path = "/" component = {Game}/>
-       {/* <IndexRoute component = {Game} /> */}
-       <Route exact path="/testing" component={TestSocket} />
-       <Route exact path="/multiplayer" component={Multiplayer} />
-       {/* <Route path = "about" component = {About} />
+      <Route exact path="/" component={Game} />
+      {/* <IndexRoute component = {Game} /> */}
+      <Route exact path="/testing" component={TestSocket} />
+      <Route exact path="/multiplayer" component={Multiplayer} />
+      {/* <Route path = "about" component = {About} />
        <Route path = "contact" component = {Contact} /> */}
-    {/* </Route> */}
- </Router>), document.getElementById("root"));
+      {/* </Route> */}
+   </Router>), document.getElementById("root"));
