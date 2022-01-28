da256306c6c64176b4200ddfc07354f1a0e74fe5
Connor Moynahan <connormoynahan@julias-mbp.lan>
2022-01-28 15:04:39 -0500

    new version

---
+++ precache-manifest.921128e34de548bad79b1d72dccaff63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6ad9033d434408d1a7ece98cbf3b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc0ba014d0703215ea0",
+    "url": "/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "680c712b983a05aca8ee",
+    "url": "/static/css/main.45317c0a.chunk.css"
+  },
+  {
+    "revision": "6dc0ba014d0703215ea0",
+    "url": "/static/js/2.91eaaf9f.chunk.js"
+  },
+  {
+    "revision": "f49f9b48ad7d1b797a8f06d032e32360",
+    "url": "/static/js/2.91eaaf9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "680c712b983a05aca8ee",
+    "url": "/static/js/main.b862467a.chunk.js"
+  },
+  {
+    "revision": "519d5f17a0662f99ef24",
+    "url": "/static/js/runtime-main.9f4d2fac.js"
+  }
+]);
